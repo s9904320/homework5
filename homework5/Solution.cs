715e8182c517d3c15ecb44ccb6c5762ec4a0df78
s9904320 <91059581+s9904320@users.noreply.github.com>
2022-02-05 16:38:30 +0800

    Add project files.

---
+++ homework5/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace homework5
+{
+    public class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] reAns = new int[2];
+            if(nums.Length == 0 || target < 0)
+            {
+                reAns[0] = 0;
+                reAns[1] = 0;
+                return reAns;
+            }
+            else
+            {
+                for(int firstNum = 0; firstNum < nums.Length; firstNum++)
+                {
+                    if((firstNum + 1) < nums.Length)
+                    {
+                        for (int secNum = firstNum + 1; secNum < nums.Length; secNum++)
+                        {
+                            if (target == (nums[firstNum] + nums[secNum]))
+                            {
+                                reAns[0] = firstNum;
+                                reAns[1] = secNum;
+                            }
+                        }
+                    }
+                }
+                return reAns;
+            }       
+        }
+    }
+}
---
+++ homework5Tests/SolutionTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework5.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void TwoSumForEmptyOrTargetNegative()
+        {
+            int[] actual = { 0, 0 };
+            int[] testNum = new int[0];
+            int[] expected = Solution.TwoSum(testNum, 9);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+            
+            int[] testNum1 = { 1, 2 };
+            expected = Solution.TwoSum(testNum1, -1);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+        [TestMethod()]
+        public void TwoSumForNormal()
+        {
+            int[] actual = { 0, 1 };
+            int[] testNum = { 2, 7, 11, 15 };
+            int[] expected = Solution.TwoSum(testNum, 9);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+
+            actual[0] = 1;
+            actual[1] = 2;
+            int[] testNum1 = { 3, 2, 4 };
+            expected = Solution.TwoSum(testNum1, 6);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+
+            actual[0] = 0;
+            actual[1] = 1;
+            int[] testNum2 = { 3, 3 };
+            expected = Solution.TwoSum(testNum2, 6);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+
+        }
+    }
+}
